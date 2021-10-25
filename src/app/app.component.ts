3353628481243c17ba3ac781bc7f4320b600d712
Ricardo Brito Brens <Ricardo_Brito@bancocaribe.com.do>
2021-10-25 12:47:34 -0400

    Work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  data: any[] = [];
   title = 'RocketAngular';
+
+  constructor( private http: HttpClient){
+
+  }
+  
+  ngOnInit(): void {
+    this.http.get('https://api.spacexdata.com/v4/rockets').subscribe((data : any)=> {
+    this.data = data;
+    })
+  }
 }
